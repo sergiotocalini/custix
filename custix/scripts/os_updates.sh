27f5809b6cbe326f8aa0e80d6a7cfddae57486de
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-03-26 17:16:26 +0200

    Updating os_updates script.

--- custix/scripts/os_updates.sh
+++ custix/scripts/os_updates.sh
@@ -4,6 +4,10 @@ type=${1}
 
 OS_FAMILY=`lsb_release -s -i 2>/dev/null` 
 if [[ ${OS_FAMILY} =~ (Ubuntu|Debian) ]]; then
+    if ! [[ -f /etc/apt/apt.conf.d/02custix ]]; then
+	echo 'APT::Periodic::Enable "1";' > /etc/apt/apt.conf.d/02custix
+	echo 'APT::Periodic::Update-Package-Lists "1";' >> /etc/apt/apt.conf.d/02custix
+    fi
     if [[ ${type} == "security" ]]; then
 	res=`apt-get -s upgrade | grep -ci ^inst.*security | tr -d '\n'`
     elif [[ ${type} == "updates" ]]; then
